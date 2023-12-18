7d372aa2c24924e1baad1f1710ce0c9392ab5db8
chirichidi <tolessnoise@gmail.com>
2023-12-18 16:38:54 +0900

    bugfix: handle image channel on cifar10 type

--- Month_2/WeekEnd/Week6/infer.py
+++ Month_2/WeekEnd/Week6/infer.py
@@ -11,6 +11,8 @@ from utils.get_module import load_image
 from utils.parser import parse_infer_args
 from utils.get_module import get_transform
 from utils.get_module import get_model
+from utils.utils import index2classname
+from utils.utils import image_check
 
 def main():
     args = parse_infer_args()
@@ -22,13 +24,17 @@ def main():
     with open(hparam_path, "r", encoding='utf-8') as f:
         train_args = argparse.Namespace(**json.load(f))
 
-    image_size = train_args.image_size
-    hidden_size = train_args.hidden_size
-    num_class = train_args.num_class
+    args.image_size = train_args.image_size
+    args.hidden_size = train_args.hidden_size
+    args.num_class = train_args.num_class
 
     image = load_image(args)
     transform = get_transform(args=args)
+
     image = transform(image).to(args.device)
+    # 이미지가 모델에 들어갈 수 있는지 체크
+    # RGB 의 경우 batch-size 를 추가하거나, grep scale 이미지의 경우 채널을 맞추는 등
+    image = image_check(image, args)
 
     model = get_model(args)
 
@@ -41,9 +47,10 @@ def main():
 
     # 결과로 나온 데이터를 해석 (시각화)
     output_index = torch.argmax(output).item()
+    _class = index2classname(output_index, args)
     prob = torch.max(nn.functional.softmax(output, dim=1)) * 100
 
-    print(f"모델은 이 이미지를 {prob:.4f}%의 확률로 {output_index+1} 라고 합니다.")
+    print(f"모델은 이 이미지를 {prob:.4f}%의 확률로 {_class} 라고 합니다.")
 
 
 if __name__ == "__main__":
--- Month_2/WeekEnd/Week6/utils/utils.py
+++ Month_2/WeekEnd/Week6/utils/utils.py
@@ -1,9 +1,25 @@
 import json
 import os
+import torch
+
+MNIST_CLASSES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+CIFAR_CLASSES = [
+    "airplane",
+    "automobile",
+    "bird",
+    "cat",
+    "deer",
+    "dog",
+    "frog",
+    "horse",
+    "ship",
+    "truck",
+]
+
 
 def make_results_dir(results_folder_path):
     if not os.path.exists(results_folder_path):
-            os.makedirs(results_folder_path)
+        os.makedirs(results_folder_path)
 
 
 def make_sub_results(args):
@@ -21,3 +37,22 @@ def save_hparams(args):
     del dict_args["device"]
     with open(os.path.join(args.save_path, "hparam.json"), "w", encoding="utf-8") as f:
         json.dump(dict_args, f, indent=4)
+
+
+def image_check(image, args):
+    if args.data == "mnist":
+        return image
+    elif args.data == "cifar10":  # 3채널 데이터
+        image = torch.unsqueeze(image, dim=0)
+        return image
+    else:
+        raise NotImplementedError
+
+
+def index2classname(index, args):
+    if args.data == "mnist":
+        return MNIST_CLASSES[index]
+    elif args.data == "cifar10":  # 3채널 데이터
+        return CIFAR_CLASSES[index]
+    else:
+        raise NotImplementedError
